f036f7702cbf86321d38e733c56ef8bd4b4a1512
AbhayR-Hestabit <abhay.rajput@hestabit.in>
2026-01-12 20:04:10 +0530

    add basic app.js for husky validation

---
+++ day5/app.js
@@ -0,0 +1,17 @@
+function getTimestamp() {
+  return new Date().toISOString();
+}
+
+function logStartup() {
+  const message = `Application started at ${getTimestamp()}`;
+  console.log(message);
+  return message;
+}
+
+if (require.main === module) {
+  logStartup();
+}
+
+module.exports = {
+  logStartup,
+};

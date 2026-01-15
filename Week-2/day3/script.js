0f1cc4d2a97256e1f41556dc803543f98cef275d
AbhayR-Hestabit <abhay.rajput@hestabit.in>
2026-01-15 19:41:55 +0530

    updated script.js

--- Week-2/day3/script.js
+++ Week-2/day3/script.js
@@ -1,9 +1,36 @@
-const faqs = document.querySelectorAll(".faq")
+const faqData = [
+{
+question: "What is Render Tree?",
+answer: "A combination of DOM tree and CSSOM Tree, without visually non appearing elements."
+},
+{
+question: "What is Layout?",
+answer: "The process of calculating position of nodes in Render tree."
+},
+{
+question: "What is URI?",
+answer: "Uniform Resource Identifier, Example: mailto:demouser@gmail.com"
+}
+]
 
-faqs.forEach((faq) => {
-const button = faq.querySelector(".question")
+const faqSection = document.querySelector('[aria-label="FAQ Section"]')
 
-button.addEventListener("click", () => {
+faqData.forEach(({ question, answer }) => {
+const faq = document.createElement("div")
+faq.className = "faq"
+
+faq.innerHTML = `
+<button class="question">${question}</button>
+<div class="answer">
+<p>${answer}</p>
+</div>
+`
+
+faqSection.appendChild(faq)
+
+const btn = faq.querySelector(".question")
+
+btn.addEventListener("click", () => {
 faq.classList.toggle("active")
 })
 })

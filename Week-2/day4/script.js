2f79774a3d093399eb424695eca30ee2684a03c3
AbhayR-Hestabit <abhay.rajput@hestabit.in>
2026-01-19 12:27:48 +0530

    finished todo

--- Week-2/day4/script.js
+++ Week-2/day4/script.js
@@ -11,23 +11,40 @@ todos = []
 }
 
 const todolist = document.getElementById("todo-list")
+const form = document.getElementById("todo-form")
+const input = document.getElementById("todo-input")
 
 const rendertodos = () => {
 todolist.innerHTML = ""
 
+if (todos.length === 0){
+todolist.innerHTML = "<li>No todos yet</li>"
+return
+}
+
 todos.forEach((todo, index) => {
 
 const li = document.createElement("li")
-li.textContent = todo
+//li.textContent = todo
+
+const span = document.createElement("span")
+span.textContent = todo.text
+
+const editBtn = document.createElement("button")
+editBtn.textContent = "Edit"
 
 const deleteBtn = document.createElement("button")
 deleteBtn.textContent = "X"
+
+editBtn.addEventListener("click", () => {
+edittodo(index)
+})
 
 deleteBtn.addEventListener("click", () => {
 deletetodo(index)
 })
 
-li.appendChild(deleteBtn)
+li.append(span, editBtn, deleteBtn)
 todolist.appendChild(li)
 })
 }
@@ -36,16 +53,13 @@ const savetodos = () => {
 localStorage.setItem("todos", JSON.stringify(todos))
 }
 
-const form = document.getElementById("todo-form")
-const input = document.getElementById("todo-input")
-
 form.addEventListener("submit", (e) => {
 e.preventDefault()
 
-const value = input.value.trim() 
+const value = input.value.trim()
 if (!value) return
 
-todos.push(value)
+todos.push({text:value})
 savetodos()
 rendertodos()
 
@@ -53,9 +67,23 @@ input.value = ""
 })
 
 const deletetodo = (index) => {
-todos.splice(index, 1)
+todos.splice(index,1)
+savetodos()
+rendertodos()
+}
+
+const edittodo = (index) => {
+const newText = prompt("Edit todo", todos[index].text)
+//const trimmed = newText.trim()
+if (!newText) return
+const trimmed = newText.trim()
+if (!trimmed) return
+todos[index].text = trimmed
+
+//todos[index].text = newText.trim()
 savetodos()
 rendertodos()
 }
 
 rendertodos()
+

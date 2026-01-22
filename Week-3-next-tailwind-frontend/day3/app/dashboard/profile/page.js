f878db1cad013134453b7f6dc8e0907402481b16
AbhayR-Hestabit <abhay.rajput@hestabit.in>
2026-01-22 15:42:45 +0530

    created profile.js

---
+++ Week-3-next-tailwind-frontend/day3/app/dashboard/profile/page.js
@@ -0,0 +1,12 @@
+export default function ProfilePage() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-semibold text-gray-100">
+        Profile
+      </h1>
+      <p className="text-gray-400">
+        This is the profile page under dashboard.
+      </p>
+    </div>
+  );
+}

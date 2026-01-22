4c7c0aefb81269297dfcc36784664d6deaf21eae
AbhayR-Hestabit <abhay.rajput@hestabit.in>
2026-01-22 15:23:21 +0530

    created about page

---
+++ Week-3-next-tailwind-frontend/day3/app/about/page.js
@@ -0,0 +1,13 @@
+export default function AboutPage() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-semibold text-gray-100">
+        About
+      </h1>
+      <p className="text-gray-400">
+        This project demonstrates routing, layouts, and reusable UI components
+        using Next.js App Router.
+      </p>
+    </div>
+  );
+}

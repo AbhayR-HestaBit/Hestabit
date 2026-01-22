276fde0f30c5b6f3914597271c3fa4009d8a7117
AbhayR-Hestabit <abhay.rajput@hestabit.in>
2026-01-22 15:53:10 +0530

    added page.js(landing page)

---
+++ Week-3-next-tailwind-frontend/day3/app/page.js
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-3xl font-bold text-gray-200">
+        Welcome to Admin Dashboard
+      </h1>
+      <p className="text-gray-400">
+        This is a sample landing page built using Next.js and Tailwind CSS.
+      </p>
+    </div>
+  );
+}
+

ba6162d5f6d922004c110755cbdda8d060318978
AbhayR-Hestabit <abhay.rajput@hestabit.in>
2026-02-03 18:45:19 +0530

    created tracing.js

---
+++ Week-4-Advanced-Backend/day5/src/utils/logger.js
@@ -0,0 +1,32 @@
+const { createLogger, format, transports } = require("winston");
+
+const requestIdFormat = format((info) => {
+  info.requestId = info.requestId || "-";
+  return info;
+});
+
+const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    requestIdFormat(),
+    format.timestamp(),
+    format.printf(({ level, message, timestamp, requestId }) => {
+      return `[${timestamp}] ${level.toUpperCase()} [${requestId}]: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.File({
+      filename: "src/logs/error.log",
+      level: "error"
+    }),
+    new transports.File({
+      filename: "src/logs/app.log"
+    })
+  ]
+});
+
+if (process.env.NODE_ENV !== "prod") {
+  logger.add(new transports.Console());
+}
+
+module.exports = logger;
